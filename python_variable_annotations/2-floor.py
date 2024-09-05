5cb29257dcb4a6ff48e9c3728d9b4206f0b1ed94
Beritha Niyotwagira <b.niyotwagi@alustudent.com>
2024-09-05 18:12:34 +0200

    done

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -6,6 +6,7 @@ a float `n` as an argument and returns the floor of the float.
 
 import math
 
+
 def floor(n: float) -> int:
     """Returns the floor of a float.
 
--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -6,6 +6,7 @@ a float `n` as an argument and returns the string representation of the float.
 
 import math
 
+
 def to_str(n: float) -> str:
     """Converts a float to its string representation.
 
